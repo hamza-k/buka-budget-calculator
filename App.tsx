5fa96e78b8c0c46cd1d4e64f77b34897f5aaaca6
Hamza KHADRI <hamza.khadri@outlook.com>
2020-05-08 23:14:59 +0200

    initializing navigation page test

--- App.tsx
+++ App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//This is an example code to pass values between two screens using React Navigator// 
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HK's Budget Calculator</Text>
-    </View>
-  );
-}
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import FirstPage from './components/FirstPage';
+import SecondPage from './components/SecondPage';
+
+//import all the screens we are going to switch 
+const App = createStackNavigator({
+  //Constant which holds all the screens like index of any book 
+    FirstPage: { screen: FirstPage }, 
+    //First entry by default be our first screen 
+    //if we do not define initialRouteName
+    SecondPage: { screen: SecondPage }, 
   },
-});
+  {
+    initialRouteName: 'FirstPage',
+  }
+);
+export default createAppContainer(App);
